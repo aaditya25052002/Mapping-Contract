bc7e3b40790a0269503602324fd5dcbd5c28993b
Aditya Tiwari <pubgmlord25@gmail.com>
2023-06-22 15:19:26 +0530

    tests

--- scripts/deploy0.js
+++ scripts/deploy0.js
@@ -1,65 +1,32 @@
-describe("TokenMapping contract", function() {
-    let ERC20Token, ERC721Token, TokenMapping, owner, addr1;
-    let erc20, erc721, tokenMapping;
-  
-    beforeEach(async function() {
-      [owner, addr1] = await ethers.getSigners();
-  
-      // Deploy ERC20 token
-      ERC20Token = await ethers.getContractFactory("ERC20Token");
-      erc20 = await ERC20Token.deploy(1000000); // deploy with initial supply
-      await erc20.deployed();
-  
-      // Deploy ERC721 token
-      ERC721Token = await ethers.getContractFactory("ERC721Token");
-      erc721 = await ERC721Token.deploy("TestToken", "TT");
-      await erc721.deployed();
-  
-      // Deploy TokenMapping
-      TokenMapping = await ethers.getContractFactory("TokenMapping");
-      tokenMapping = await TokenMapping.deploy(erc721.address, erc20.address);
-      await tokenMapping.deployed();
-    });
-  
-    it("Should create a new token and its mapping correctly", async function() {
-      // Create token and its mapping
-      await tokenMapping.createTokenAndMapping(owner.address, 1);
-      const erc20Amount = await tokenMapping.getERC20Amount(1);
-      expect(erc20Amount).to.equal(1000);
-    });
-  
-    it("Should fail if the same ERC721 token id is used", async function() {
-      await tokenMapping.createTokenAndMapping(owner.address, 1);
-  
-      // The second time we try to create a mapping with the same id, it should fail
-      await expect(tokenMapping.createTokenAndMapping(owner.address, 1)).to.be.revertedWith("ERC721: token already minted");
-    });
-  
-    it("Should fail if a non-owner tries to create a mapping", async function() {
-      await expect(tokenMapping.connect(addr1).createTokenAndMapping(addr1.address, 1)).to.be.revertedWith("Ownable: caller is not the owner");
-    });
-  
-    it("Should allow owner to transfer ERC721 token", async function() {
-      await tokenMapping.createTokenAndMapping(owner.address, 1);
-  
-      await erc721.transferFrom(owner.address, addr1.address, 1);
-      const newOwner = await erc721.ownerOf(1);
-      expect(newOwner).to.equal(addr1.address);
-    });
-  
-    it("Should update ERC20 amount upon ERC721 transfer", async function() {
-      await tokenMapping.createTokenAndMapping(owner.address, 1);
-  
-      // Check initial ERC20 balance of addr1
-      let addr1Balance = await erc20.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(0);
-  
-      // Transfer ERC721 token to addr1
-      await erc721.transferFrom(owner.address, addr1.address, 1);
-  
-      // Check updated ERC20 balance of addr1
-      addr1Balance = await erc20.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(1000);
-    });
+const { ethers } = require("hardhat");
+async function main() {
+  // const [deployer] = await ethers.getSigners();
+
+  // console.log("Deploying contracts with the account:", deployer.address);
+
+  const ERC20 = await ethers.getContractFactory("ERC20Token");
+  const ERC721 = await ethers.getContractFactory("ERC721Token");
+  const ERC20Instance = await ERC20.deploy(1000000);
+  await ERC20Instance.deployed();
+
+  const ERC721Instance = await ERC721.deploy("ERC721Token", "TOKEN");
+  await ERC721Instance.deployed();
+
+  const TokenMapping = await ethers.getContractFactory("TokenMapping");
+  const mappingInstance = await TokenMapping.deploy(
+    ERC721Instance.address,
+    ERC20Instance.address
+  );
+  await mappingInstance.deployed();
+
+  console.log("ERC20 deployed to:", ERC20Instance.address);
+  console.log("ERC721 deployed to:", ERC721Instance.address);
+  console.log("TokenMapping deployed to:", mappingInstance.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
   });
-  
--- test/TokenMapping.js
+++ test/TokenMapping.js
@@ -1,29 +1,68 @@
 const { expect } = require("chai");
 
 describe("TokenMapping contract", function() {
-  it("Should create a new token and its mapping correctly", async function() {
-    const [owner] = await ethers.getSigners();
+  let ERC20Token, ERC721Token, TokenMapping, owner, addr1;
+  let erc20, erc721, tokenMapping;
+
+  beforeEach(async function() {
+    [owner, addr1] = await ethers.getSigners();
 
     // Deploy ERC20 token
-    const ERC20Token = await ethers.getContractFactory("ERC20Token");
-    const erc20 = await ERC20Token.deploy(1000000); // deploy with initial supply
+    ERC20Token = await ethers.getContractFactory("ERC20Token");
+    erc20 = await ERC20Token.deploy(1000000); // deploy with initial supply
     await erc20.deployed();
 
     // Deploy ERC721 token
-    const ERC721Token = await ethers.getContractFactory("ERC721Token");
-    const erc721 = await ERC721Token.deploy("TestToken", "TT");
+    ERC721Token = await ethers.getContractFactory("ERC721Token");
+    erc721 = await ERC721Token.deploy("TestToken", "TT");
     await erc721.deployed();
 
     // Deploy TokenMapping
-    const TokenMapping = await ethers.getContractFactory("TokenMapping");
-    const tokenMapping = await TokenMapping.deploy(erc721.address, erc20.address);
+    TokenMapping = await ethers.getContractFactory("TokenMapping");
+    tokenMapping = await TokenMapping.deploy(erc721.address, erc20.address);
     await tokenMapping.deployed();
+  });
 
+  it("Should create a new token and its mapping correctly", async function() {
     // Create token and its mapping
     await tokenMapping.createTokenAndMapping(owner.address, 1);
     const erc20Amount = await tokenMapping.getERC20Amount(1);
-
     expect(erc20Amount).to.equal(1000);
   });
+
+  it("Should fail if the same ERC721 token id is used", async function() {
+    await tokenMapping.createTokenAndMapping(owner.address, 1);
+
+    // The second time we try to create a mapping with the same id, it should fail
+    await expect(tokenMapping.createTokenAndMapping(owner.address, 1)).to.be.revertedWith("ERC721: token already minted");
+  });
+
+  it("Should fail if a non-owner tries to create a mapping", async function() {
+    await expect(tokenMapping.connect(addr1).createTokenAndMapping(addr1.address, 1)).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("Should allow owner to transfer ERC721 token", async function() {
+    await tokenMapping.createTokenAndMapping(owner.address, 1);
+
+    await erc721.transferFrom(owner.address, addr1.address, 1);
+    const newOwner = await erc721.ownerOf(1);
+    expect(newOwner).to.equal(addr1.address);
+  });
+
+  it("Should update ERC20 amount upon ERC721 transfer", async function() {
+    await tokenMapping.createTokenAndMapping(owner.address, 1);
+
+    // Check initial ERC20 balance of addr1
+    let addr1Balance = await erc20.balanceOf(addr1.address);
+    expect(addr1Balance).to.equal(0);
+
+    // Transfer ERC721 token to addr1
+    await erc721.transferFrom(owner.address, addr1.address, 1);
+
+    // Check updated ERC20 balance of addr1
+    addr1Balance = await erc20.balanceOf(addr1.address);
+    expect(addr1Balance).to.equal(1000);
+  });
 });
+
 
